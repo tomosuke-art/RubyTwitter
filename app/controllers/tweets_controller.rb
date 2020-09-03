31b84a7b533a0a2768d4e91e424e751f3845eb2b
tomosuke-art <tomo1130taro@icloud.com>
2020-09-04 02:02:56 +0900

    投稿機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,37 @@ class TweetsController < ApplicationController
   def index
     
   end
+
+  def new
+    @tweet = Tweet.new #Tweetの内容をまとめている情報
+    # @ をつけることでviewsで使用できる
+  end
+
+  # def create
+  #   @tweet = Tweet.new(tweet_params)  #代入
+  #   if @tweet.save
+  #     redirect_to root_path
+  #   else 
+  #     redirect_to tweets_new_path
+  #   end
+  # end
+
+  # private
+  # def tweet_params
+  #   params.require(:tweet).permit(:body)
+  # end
+  def create
+    @tweet = Tweet.new(tweet_params) # フォームに入力された情報をもとにtweetを生成
+    if @tweet.save
+      redirect_to :action => "index"
+    else
+      redirect_to :action => "new"
+    end
+  end
+
+  private
+  def tweet_params # 受け取りたい情報だけに絞り込んでいる
+    params.permit(:body)
+    # require(:tweet) -> 不要
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'hello/index' => 'hello#index' 
+  root 'hello#index' 
   get 'hello/link' => 'hello#link'
   get 'tweets' => 'tweets#index'
+  get 'tweets/new' => 'tweets#new'
+  post 'tweets' => 'tweets#create'
+
 end
 
